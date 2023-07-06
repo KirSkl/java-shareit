b4ca0728944d02f6be54ec388d07d9bcee387595
Kirill Skladanov <wra1th@inbox.ru>
2023-07-06 15:39:51 +0300

    <feat> заполнить класс юзер, создать базовый контроллер

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.user;
 
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
 public class User {
+    private long id;
+    @NotBlank(message = "Имя не может быть пустым")
+    private String name;
+    @Email(message = "Некорректный email")
+    private String email;
 }
--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -1,12 +1,38 @@
 package ru.practicum.shareit.user;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.exceptions.NotFoundException;
 
-/**
- * TODO Sprint add-controllers.
- */
+import java.util.HashMap;
+
 @RestController
 @RequestMapping(path = "/users")
 public class UserController {
+    HashMap<Long, User> users = new HashMap<>();
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        users.put(user.getId(), user);
+        return user;
+    }
+
+    @PutMapping
+    public User updateUser(@RequestBody User user) {
+        if (users.containsKey(user.getId())) {
+            users.put(user.getId(), user);
+            return user;
+        } else {
+            throw new NotFoundException("Пользователь с таким ID не найден");
+        }
+    }
+
+    @DeleteMapping({"/id"})
+    public void deleteUser(@PathVariable(name = "id") long id) {
+        if (users.containsKey(id)) {
+            users.remove(id);
+        } else {
+            throw new NotFoundException("Пользователь с таким ID не найден");
+        }
+    }
+
 }
