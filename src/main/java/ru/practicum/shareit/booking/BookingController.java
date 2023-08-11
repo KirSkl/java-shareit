dcb837c91bdcf90e7769f255613e3bafaf321dda
Kirill Skladanov <wra1th@inbox.ru>
2023-08-11 14:51:43 +0300

    <fix> исправить логи, убрать лишние импорты

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -3,13 +3,9 @@ package ru.practicum.shareit.booking;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.booking.dto.BookingDtoPatchResponse;
 import ru.practicum.shareit.booking.dto.BookingDtoRequest;
 import ru.practicum.shareit.booking.dto.BookingDtoResponse;
-import ru.practicum.shareit.booking.model.Booking;
-import ru.practicum.shareit.booking.model.BookingStates;
 import ru.practicum.shareit.common.Validator;
-import ru.practicum.shareit.exceptions.UnsupportedBookingStateException;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -26,7 +22,7 @@ public class BookingController {
     @PostMapping
     public BookingDtoResponse createBooking(@RequestHeader("X-Sharer-User-Id") Long userId,
                                             @Valid @RequestBody BookingDtoRequest bookingDtoRequest) {
-        log.info(String.format("Получен запрос на бронирование от пользователя с id = %s", userId));
+        log.info(String.format("Получен запрос POST /bookings на бронирование от пользователя с id = %s", userId));
         validator.validateId(userId);
         validator.validateBookingDto(bookingDtoRequest);
         return bookingService.createBooking(userId, bookingDtoRequest);
@@ -34,9 +30,9 @@ public class BookingController {
 
     @PatchMapping("/{bookingId}")
     public BookingDtoResponse approvedBooking(@PathVariable Long bookingId, @RequestParam Boolean approved,
-                                                   @RequestHeader("X-Sharer-User-Id") Long userId) {
-        log.info(String.format("Получен запрос на изменение статуса бронирования id = %s от пользователя" +
-                "id = %s", bookingId, userId));
+                                              @RequestHeader("X-Sharer-User-Id") Long userId) {
+        log.info(String.format("Получен запрос PATCH /bookingId = %s на изменение статуса бронирования  от " +
+                "пользователя id = %s", bookingId, userId));
         validator.validateId(userId);
         validator.validateId(bookingId);
         return bookingService.approvedBooking(bookingId, approved, userId);
@@ -45,8 +41,8 @@ public class BookingController {
     @GetMapping("/{bookingId}")
     public BookingDtoResponse getBooking(@RequestHeader("X-Sharer-User-Id") Long userId,
                                          @PathVariable Long bookingId) {
-        log.info(String.format("Получен запрос на получение информации о бронировании id = %s " +
-                "от пользователя id = %s", bookingId, userId));
+        log.info(String.format("Получен запрос GET/bookingId = %s на получение информации о бронировании от " +
+                "пользователя id = %s", bookingId, userId));
         validator.validateId(userId);
         validator.validateId(bookingId);
         return bookingService.getBooking(userId, bookingId);
@@ -54,16 +50,18 @@ public class BookingController {
 
     @GetMapping
     public List<BookingDtoResponse> getAllBookings(@RequestHeader("X-Sharer-User-Id") Long userId,
-                                               @RequestParam(defaultValue = "ALL") String state) {
-         log.info(String.format("Получен запрос на получение всех бронирований пользователя с id = %s", userId));
-         validator.validateId(userId);
-         return bookingService.getAllBookings(userId, state);
+                                                   @RequestParam(defaultValue = "ALL") String state) {
+        log.info(String.format("Получен запрос GET/bookings на получение всех бронирований пользователя с id = %s",
+                userId));
+        validator.validateId(userId);
+        return bookingService.getAllBookings(userId, state);
     }
 
     @GetMapping("/owner")
     public List<BookingDtoResponse> getAllItemBookings(@RequestHeader("X-Sharer-User-Id") Long userId,
-                                                   @RequestParam(defaultValue = "ALL") String state) {
-        log.info(String.format("Получен запрос на получение всех бронирований вещей пользователя с id = %s", userId));
+                                                       @RequestParam(defaultValue = "ALL") String state) {
+        log.info(String.format("Получен запрос GET/owner на получение всех бронирований вещей пользователя с id = %s",
+                userId));
         validator.validateId(userId);
         return bookingService.getAllItemBookings(userId, state);
     }
