376bfa0af92090390beab655317bc3f9cdc2c5b5
Kirill Skladanov <wra1th@inbox.ru>
2023-08-24 20:18:15 +0300

    <feat> тесты 100/100

--- src/test/java/ru/practicum/shareit/user/UserControllerTest.java
+++ src/test/java/ru/practicum/shareit/user/UserControllerTest.java
@@ -4,26 +4,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.InOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.practicum.shareit.common.Validator;
-import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.exceptions.ValidationIdException;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
-import java.util.Set;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -42,14 +39,12 @@ public class UserControllerTest {
     private Long userId;
     private User user;
     private UserDto userDto;
-    private Long wrongId;
 
     @BeforeEach
     void loadInitial() {
         userId = 1L;
         userDto = new UserDto(1L, "John Doe", "John_Doe@mail.com");
         user = new User(userDto.getId(), userDto.getName(), userDto.getEmail());
-        wrongId = -1L;
     }
 
     @SneakyThrows
@@ -73,10 +68,107 @@ public class UserControllerTest {
         when(service.getUser(userId)).thenReturn(userDto);
 
         mvc.perform(get("/users/{userId}", userId)
-                .accept(MediaType.APPLICATION_JSON))
+                        .accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", is(userDto.getId()), Long.class))
                 .andExpect(jsonPath("$.name", is(userDto.getName())))
                 .andExpect(jsonPath("$.email", is(userDto.getEmail())));
+        InOrder order = inOrder(validator, service);
+        order.verify(validator, times(1)).validateId(userId);
+        order.verify(service, times(1)).getUser(userId);
+    }
+
+    @SneakyThrows
+    @Test
+    void testGetUserInvalidIdThrown() {
+        doThrow(new ValidationIdException("Неверный id")).when(validator).validateId(userId);
+
+        mvc.perform(get("/users/{userId}", userId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        verify(service, never()).getUser(anyLong());
+    }
+
+    @SneakyThrows
+    @Test
+    void testCreateUserOk() {
+        when(service.createUser(user)).thenReturn(userDto);
+
+        mvc.perform(post("/users")
+                        .contentType("application/json")
+                        .content(mapper.writeValueAsString(userDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(userDto.getName())))
+                .andExpect(jsonPath("$.email", is(userDto.getEmail())));
+    }
+
+    @SneakyThrows
+    @Test
+    void testCreateUserNameIsBlankThrown() {
+        user.setName("");
+
+        mvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        verify(service, never()).createUser(any());
+    }
+
+    @SneakyThrows
+    @Test
+    void testCreateUserWrongEmailThrown() {
+        user.setEmail("wrongEmail");
+
+        mvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        verify(service, never()).createUser(any());
+    }
+
+    @SneakyThrows
+    @Test
+    void testUpdateUserOk() {
+        when(service.updateUser(userId, user)).thenReturn(userDto);
+
+        mvc.perform(patch("/users/{userId}", userId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(userDto))
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(userDto.getId()), Long.class))
+                .andExpect(jsonPath("$.name", is(userDto.getName())))
+                .andExpect(jsonPath("$.email", is(userDto.getEmail())));
+
+        verify(service, times(1)).updateUser(userId, user);
+    }
+
+    @SneakyThrows
+    @Test
+    void testUpdateUserEmailIsBlankThrown() {
+        user.setEmail("");
+
+        mvc.perform(patch("/users/{userId}", userId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+
+        verify(service, never()).updateUser(anyLong(), any());
+    }
+
+    @SneakyThrows
+    @Test
+    void testDeleteUserOk() {
+        mvc.perform(delete("/users/{userId}", userId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        verify(service, times(1)).deleteUser(userId);
     }
 }
