dcba974e0d42b2632e63a0b78faeb2e1bde83319
Kirill Skladanov <wra1th@inbox.ru>
2023-08-26 10:32:31 +0300

    <test> покрыть тестами кастомные методы

--- src/test/java/ru/practicum/shareit/item/CommentRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/item/CommentRepositoryTest.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -12,18 +11,16 @@ import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
 public class CommentRepositoryTest {
     @Autowired
-    private CommentRepository repository;
-    @Autowired
     ItemRepository itemRepository;
     @Autowired
     UserRepository userRepository;
+    @Autowired
+    private CommentRepository repository;
 
     @Test
     void testFindAllByItemOk() {
