e01c133d5bdcae370308fb421e2a4fad3aafb2ff
Kirill Skladanov <wra1th@inbox.ru>
2023-09-19 09:31:54 +0300

    <fix> все тесты в постмане пройдены

--- gateway/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ gateway/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -19,14 +19,14 @@ import javax.validation.constraints.PositiveOrZero;
 @RequestMapping(path = "/requests")
 @Validated
 public class ItemRequestController {
-    private final RequestClient requestClient;
+    private final ItemRequestClient itemRequestClient;
 
     @PostMapping
     public ResponseEntity<Object> addRequest(@Positive @RequestHeader(Constants.USER_HEADER) Long userId,
                                              @Valid @RequestBody ItemRequestDto itemRequestDto) {
         log.info(String.format(
                 "Получен запрос POST /requests на добавление нового запроса вещи от пользователя с id = %s", userId));
-        return requestClient.addRequest(userId, itemRequestDto);
+        return itemRequestClient.addRequest(userId, itemRequestDto);
     }
 
     @GetMapping
@@ -34,7 +34,7 @@ public class ItemRequestController {
             @Positive @RequestHeader(Constants.USER_HEADER) Long userId) {
         log.info(String.format(
                 "Получен запрос GET /requests на получение списка своих запросов от пользователя с id = %s", userId));
-        return requestClient.getMyRequests(userId);
+        return itemRequestClient.getMyRequests(userId);
     }
 
     @GetMapping("/{requestId}")
@@ -42,7 +42,7 @@ public class ItemRequestController {
                                                   @Positive @RequestHeader(Constants.USER_HEADER) Long userId) {
         log.info(String.format("Получен запрос GET /requests/requestId=%s на получение информации о запросе",
                 requestId));
-        return requestClient.findItemRequest(requestId, userId);
+        return itemRequestClient.findItemRequest(requestId, userId);
     }
 
     @GetMapping("/all")
@@ -53,6 +53,6 @@ public class ItemRequestController {
         log.info(String.format(
                 "Получен запрос GET /requests/all от пользователя = %s на получение списка запросов других " +
                         "пользователей с параметрами пагинации от %s до %s", userId, from, size));
-        return requestClient.getAll(from, size, userId);
+        return itemRequestClient.getAll(from, size, userId);
     }
 }
