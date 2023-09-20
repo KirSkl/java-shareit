69a03c96d7aa17d913882f037dbd56456dc4fbdc
Kirill Skladanov <wra1th@inbox.ru>
2023-09-20 13:34:51 +0300

    <fix> изменить аннотации класса

--- gateway/src/main/java/ru/practicum/shareit/common/Constants.java
+++ gateway/src/main/java/ru/practicum/shareit/common/Constants.java
@@ -3,11 +3,10 @@ package ru.practicum.shareit.common;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
+import lombok.experimental.UtilityClass;
 
-@Getter
-@Data
-@AllArgsConstructor
-public class Constants {
+@UtilityClass
+public final class Constants {
     public static final String USER_HEADER = "X-Sharer-User-Id";
     public static final String DEFAULT_FROM = "0";
     public static final String DEFAULT_SIZE = "10";
